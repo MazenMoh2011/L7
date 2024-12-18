2d1fb8acf451ef94987449fbefa7ba24e51e062a
MazenMoh2011 <mazenalnagar203@gmail.com>
2024-12-18 10:47:32 +0300

    dfasadfsdfasfasdsdfaadfsffffdfffffffffffffffdddddddddddddddfffffffffff

---
+++ A3.py
@@ -0,0 +1,21 @@
+ans= int(input("enter a number: "))
+a= print("a.add")
+b= print("b.subtract")
+c= print("c.multiplication")
+d= print("d.division")
+if ans==a:
+    ans2= int(input("choose your first number: "))
+    ans3= int(input("choose a second number: "))
+    ans2+ans3
+if ans==b:
+    ans4= int(input("choose your first number: "))
+    ans5= int(input("choose a second number: "))
+    ans4+ans5
+if ans==c:
+    ans6= int(input("choose your first number: "))
+    ans7= int(input("choose a second number: "))
+    ans6*ans7
+if ans==d:
+    ans8= int(input("choose your first number: "))
+    ans9= int(input("choose a second number: "))
+    ans8/ans9
